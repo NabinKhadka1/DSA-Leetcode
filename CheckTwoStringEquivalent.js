45ea44dbd813b6800a9f13a814a1436ba307620b
NabinKhadka1 <nabinkhadka330@gmail.com>
2024-01-20 00:00:22 +0545

    Day 11: Leetcode Check if two strings are equivalent

---
+++ CheckTwoStringEquivalent.js
@@ -0,0 +1,27 @@
+// Given two string arrays word1 and word2, return true if the two arrays represent the same string, and false otherwise.
+
+// A string is represented by an array if the array elements concatenated in order forms the string.
+
+ 
+
+// Example 1:
+
+// Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+// Output: true
+// Explanation:
+// word1 represents string "ab" + "c" -> "abc"
+// word2 represents string "a" + "bc" -> "abc"
+// The strings are the same, so return true.
+
+var arrayStringsAreEqual = function(word1, word2) {
+    let sumWord1 = '';
+    let sumWord2 = '';
+    for(let i=0; i<word1.length; i++){
+        sumWord1 += word1[i]
+    }
+    for(let i=0; i<word2.length; i++){
+        sumWord2 += word2[i]
+    }
+    return sumWord1 === sumWord2;
+    
+};
